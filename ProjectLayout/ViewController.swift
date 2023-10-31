2a8fbd714022335e9fa9ef063fa2a2ed5094d696
Edris ILYAS <idrsilyas@gmail.com>
2023-10-31 23:11:09 +0300

    Layout Test

---
+++ ProjectLayout/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ProjectLayout
+//
+//  Created by Edris ILYAS on 22.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    
+
+
+}
+
